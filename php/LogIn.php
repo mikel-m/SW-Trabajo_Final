5f7ea4753f749fd66c1dba9891fb155dc3e99f3a
mikel-m <mmorillo005@ikasle.ehu.eus>
2021-11-29 15:21:08 +0000

    añadir br

--- php/LogIn.php
+++ php/LogIn.php
@@ -67,13 +67,13 @@
               // comprobar password cifrado
               //https://programacionconphp.com/encriptar-contrasena-en-php/
               $hash = password_hash($userpass, PASSWORD_DEFAULT);
-              echo 'hash: ' . $hash;
+              echo 'hash: ' . $hash . '<br>';
               $sql = "SELECT * from users where correo = '$correo'";
               $logear = mysqli_query($conn, $sql) or die(mysqli_error($conn));
               $row = mysqli_fetch_array($logear, MYSQLI_ASSOC) ; //Lo convertimos a array
 
               //if(!$row){
-              echo 'row: ' . $row['pass'];
+              echo 'row: ' . $row['pass'] . '<br>';
               if(!password_verify($row['pass'], $hash)){
                 echo "<h3>Datos de login incorrectos. :(</h3>";
                 echo "<br>";
