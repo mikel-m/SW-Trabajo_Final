bfd1f77231c58cf799a7d23182d6e1aa36263db1
mikel-m <mmorillo005@ikasle.ehu.eus>
2021-12-01 12:11:54 +0000

    pass verify

--- php/LogIn.php
+++ php/LogIn.php
@@ -75,7 +75,7 @@
               echo 'row: ' . $row['pass'] . '<br>';
               $pass_verify = password_verify($userpass, $row['pass']);
               echo 'password_verify: ' . $pass_verify . '<br>';
-              if(password_verify($userpass, $row['pass'])){
+              if($pass_verify != 1){
                 echo "<h3>Datos de login incorrectos. :(</h3>";
                 echo "<br>";
               }
