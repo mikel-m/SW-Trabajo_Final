4a42b32109182733c51f269f1a7be97775774fb4
mikel-m <mmorillo005@ikasle.ehu.eus>
2021-12-10 11:52:24 +0000

    PDO remove user

--- php/RemoveUser.php
+++ php/RemoveUser.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
     if(isset($_SESSION['tipo']) && $_SESSION['tipo'] == "admin"){
-
+        /*
         include "DbConfig.php";
 
         $correo = $_POST['correo'];
@@ -25,6 +25,36 @@
         }
 
         mysqli_close($mysqli);
+
+        echo "<script>
+            window.location.href='HandlingAccounts.php';
+            </script>";
+        */
+
+        
+        // PDO
+        include "DbConfig.php";
+
+        $correo = $_POST['correo'];
+
+        if ($correo == "admin@ehu.es"){
+            exit("Imposible ejecutar esta acción");
+        }
+
+        try{
+            $dsn = "mysql:host=$server;dbname=$basededatos";
+            $dbh = new PDO($dsn, $user, $pass);
+        } catch (PDOException $e){
+            echo $e->getMessage();
+        }
+        // prepare
+        $stmt = $dbh->prepare("DELETE FROM users WHERE correo=?");
+        // bind
+        $stmt->bindParam(1, $correo);
+        // execute
+        $stmt->execute();
+        // cerrar conexión
+        $dbh = null;
 
         echo "<script>
             window.location.href='HandlingAccounts.php';
