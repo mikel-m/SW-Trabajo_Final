4dea49a012e0c7192ae08d9404227a3d3d51de83
mikel-m <mmorillo005@ikasle.ehu.eus>
2021-12-13 16:29:15 +0000

    boton preguntas totales

---
+++ js/CantidadPreguntas.js
@@ -0,0 +1,25 @@
+function numQuestions(){
+    var preguntasTotales='<caption style="text-align:left"><td align="left"><h2>Número de preguntas TOTALES: ';
+    var preguntasUsuario='<caption style="text-align:left"><td align="left"><h2>Preguntas de ';
+    var correo = $('#correo').val();
+    preguntasUsuario+=correo+': '
+    var cantTotales = 0;
+    var CantUsuario = 0;
+    
+	
+    $.ajax({
+        url:"../json/Questions.json",
+        dataType: "json",
+        type: "get",
+        cache : false,
+        success: function(data){
+            $(data.assessmentItems).each(function(index,value){
+                cantTotales = cantTotales + 1;
+
+            });
+            preguntasTotales+=cantTotales+'</h2><br></caption>';
+    
+            document.getElementById('mostrar-Preguntas').innerHTML=preguntasTotales;
+        }
+    });
+}
